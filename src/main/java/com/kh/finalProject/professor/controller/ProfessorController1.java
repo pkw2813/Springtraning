cc04c317ffcc64525006854692dd4c3aef715436
jisaap <odo919@naver.com>
2019-10-29 15:59:24 +0900

    commit

---
+++ src/main/java/com/kh/finalProject/professor/controller/ProfessorController1.java
@@ -0,0 +1,17 @@
+package com.kh.finalProject.professor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfessorController1 {
+
+	@RequestMapping("")
+	public String insertSubject() {
+		
+		
+		
+		return "";
+	}
+	
+}
