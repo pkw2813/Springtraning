ac6f8f97f2094e6bf2dc6a52e634c25ec4c9f667
Moonsejun <junscent@naver.com>
2019-10-31 16:38:33 +0900

    123

--- src/main/java/com/kh/finalProject/employee/model/dao/EmployeeDaoImpl.java
+++ src/main/java/com/kh/finalProject/employee/model/dao/EmployeeDaoImpl.java
@@ -1,5 +1,8 @@
 package com.kh.finalProject.employee.model.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class EmployeeDaoImpl implements EmployeeDao {
 
 }
--- src/main/java/com/kh/finalProject/professor/model/dao/ProfessorDaoImpl.java
+++ src/main/java/com/kh/finalProject/professor/model/dao/ProfessorDaoImpl.java
@@ -1,5 +1,8 @@
 package com.kh.finalProject.professor.model.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProfessorDaoImpl implements ProfessorDao {
 
 }
