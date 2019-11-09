ab375cefc931f4c9cc5bbf7e810532cc449b7f01
tjdtnfdl <51364419+tjdtnfdl@users.noreply.github.com>
2019-11-09 22:51:25 +0900

    수강생 조회 객체 추가 Select_SubjectNameCode

---
+++ src/main/java/com/kh/finalProject/professor/model/vo/Select_SubjectNameCode.java
@@ -0,0 +1,15 @@
+package com.kh.finalProject.professor.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Select_SubjectNameCode {
+	
+	private String subCode;
+	private String subName;
+	
+}
