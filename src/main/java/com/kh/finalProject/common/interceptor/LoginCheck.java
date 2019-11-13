7c527790da5bd93a6a75d6048b300269eed0a2ed
Junsun <greendev173@gmail.com>
2019-11-14 07:18:56 +0900

    [준순] 인터셉터 등록하기 완료. 사용자가 로그인 안한 상태에서 /student/*, /prof/*, *.do로 접속하면 "로그인 후 이용가능합니다."라는 메세지 띄어주고 로그인 화면으로 전환됨.

---
+++ src/main/java/com/kh/finalProject/common/interceptor/LoginCheck.java
@@ -0,0 +1,25 @@
+package com.kh.finalProject.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginCheck extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if(request.getSession().getAttribute("loginMember")==null) {
+			request.setAttribute("msg", "로그인 후 이용가능합니다.");
+			request.setAttribute("loc", "/"); // main 화면으로 이동
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+			// HandlerMapping에 있는 것만 resolver로 통과한다.
+			return false;
+		}
+		return super.preHandle(request, response, handler);
+	}
+	
+	
+
+}
