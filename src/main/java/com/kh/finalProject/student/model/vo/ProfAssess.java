dca2802dd447b336ea4b74cb6f1941846f1f1586
taeyeong0814 <lhlhgodz3@naver.com>
2019-11-18 20:23:49 +0900

    강의 평가 모델 추가

---
+++ src/main/java/com/kh/finalProject/student/model/vo/ProfAssess.java
@@ -0,0 +1,21 @@
+package com.kh.finalProject.student.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProfAssess {
+
+	private String acaYearSem;
+	private String subCode;
+	private String profId;
+	private Date evalDate;
+	private String evalComment;
+	private int evalPoint;
+}
